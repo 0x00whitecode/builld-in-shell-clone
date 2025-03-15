97c20939e0da4b57dc7c995f93ebb49ce974f32e
0x00whitecode <x10tion007@gmail.com>
2025-03-15 06:16:32 +0100

    fix remove uncomement

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,7 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    
+    // print invalide command here 
+    println!("{}: command not found", input.trim());
 }
