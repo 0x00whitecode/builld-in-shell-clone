be806c2c0069b39ad7c12614a67d47772b08fd52
0x00whitecode <x10tion007@gmail.com>
2025-03-15 12:12:45 +0100

    addde  type handling

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::{self, Write};
 use std::process;
 
 fn main() {
+    let builtins = ["echo", "exit", "type"];
     // Uncomment this block to pass the first stage
    loop {
     print!("$ ");
@@ -18,6 +19,14 @@ fn main() {
         "exit 0" => break,
         // add the echo to this 
         input if input.starts_with("echo ") => println!("{}", &input[5..]),
+        input if input.starts_with("type ") => {
+            let command = &input[5..];
+            if builtins.contains(&command){
+                println!("{} is a shell builtin", command);
+            }else{
+                println!("{}: not found", command);
+            }
+        }
         &_ => {
             println!("{}: command not found", input.trim());
         }
